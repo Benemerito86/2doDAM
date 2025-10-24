fd8c2e557d59061b283ff751c76a6def93515114
Miguel Benjumea <benemerito.benjumea@gmail.com>
2025-10-24 18:22:58 +0200

    Add files via upload

---
+++ desarrollo_de_interfaces/mvvc_mpf/WpfAppNoSteam/Models/Juego.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppNoSteam.Models
+{
+    internal class Juego
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string Estado { get; set; } = "venta"; 
+
+  
+        public string NombrePrecio => $"{Nombre} - {Precio:F2}€";
+        public string ImagenPath => $"/{Nombre}.png";
+
+    }
+}

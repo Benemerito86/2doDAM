fcb33d121a885f546d64b686605eddce77478ed2
Miguel Benjumea <benemerito.benjumea@gmail.com>
2025-10-27 20:01:49 +0100

    Delete acceso_a_datos/Adivina_Pelicula_Serie/AdivinaPelicula directory

--- acceso_a_datos/Adivina_Pelicula_Serie/AdivinaPelicula/controllers/Score_controller.py
+++
@@ -1,5 +0,0 @@
-from models.Score import Score
-
-def guardar_puntuacion(nombre_usuario, puntuacion):
-    puntuacion2 = Score(nombre_usuario, puntuacion)
-    puntuacion2.guardar()
--- acceso_a_datos/Adivina_Pelicula_Serie/AdivinaPelicula/main.py
+++
@@ -1,9 +0,0 @@
-from db.database import inicializar_bd
-from views.Main_window import MainWindow
-import tkinter as tk
-
-if __name__ == "__main__":
-    inicializar_bd()
-    raiz = tk.Tk()
-    app = MainWindow(raiz)
-    raiz.mainloop()
